8511dc960cc60bc23bfd5e0dfa917719d515bdb9
Álvaro Quevedo Júnior <alvaro.junior@andritz.com>
2020-06-08 08:55:55 -0300

    started a app

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,23 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Header from './Header';
-
-//JSX : sintage xml dentro do javascript
+import Home from './pages/Home';
 
 function App() {
-  const [counter, setCounter] = useState(0);//[valorDoEstado, funcao para atualizar o valor do estado] 
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header title="teste" />
-      <Header title={`Contador: ${counter}`} />
-      <h1>{counter} {counter * 2}</h1>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
-    </div>
+      <Home/>
   );
 }
 
